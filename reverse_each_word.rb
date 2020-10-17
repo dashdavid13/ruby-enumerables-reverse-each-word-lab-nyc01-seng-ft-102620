4ddd8bab7652bf0467a32d434c7809397eb8dfd6
David Medina <davidamedina619@gmail.com>
2020-10-17 17:03:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(sentence)
- reversed = []
+ 
  sentence.split.each do |word|
    word.reverse
-   
-   sentence.split.map do |word|
-    word.reverse
-  end).join(' ')
- end 
+ end.join(' ')
+ 
 end 
